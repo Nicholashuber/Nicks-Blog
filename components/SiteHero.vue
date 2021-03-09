574a6bb48697e723177e30bfc268fe721508779e
Nicholas Huber <Nick@huber.dev>
2021-03-08 19:28:31 -0700

    Update SiteHero.vue

--- components/SiteHero.vue
+++ components/SiteHero.vue
@@ -9,10 +9,10 @@
     <div class="hero-body">
       <div class="container">
         <h1 class="title animated fadeInUp">
-          {{ title }} gg
+          {{ title }}
         </h1>
         <h2 class="subtitle animated fadeInUp slower">
-          {{ subtitle }} yo
+          {{ subtitle }} 
         </h2>
         <br />
         <div
@@ -24,7 +24,7 @@
 
 
 
-<div>{{ $route.name }}</div>
+<!--<div>{{ $route.name }}</div>-->
  
 <div v-if="$route.name === 'index'" data-v-2f15ddaa="" class="under-subtitle animated fadeInDown slower"><div data-v-64d22838=""><div data-v-3d9d9137="" class="grid grid-theme-boxes"><!----> <div data-v-3d9d9137="" class="columns items is-multiline" style="
     margin-left: 16.66667%;
@@ -50,9 +50,11 @@
               is-home
               has-text-weight-light
               ">
-            Getting Started
-          </h3> <h4 style="color: black !important;" data-v-28f55286="" class="subtitle is-6"><span class="author-wrapper"><strong style="color: black !important;">Author:</strong> Daniel Kelly |
-  </span> <span class="date-wrapper"><strong style="color: black !important;">Published on:</strong> August 3, 2019
+            What im Working on
+          </h3> <h4 style="color: black !important;" data-v-28f55286="" class="subtitle is-6"><span class="author-wrapper"><strong style="color: black !important;">Monthly Dev Discussion!</strong></span> <br>
+<br><span class="date-wrapper"><a class="button is-outlined is-small" style="color: #fff;background-color: #007bff;border-color: #007bff;">
+    Load Comments
+  </a>
   </span></h4></a></div></div></div></div></div><div data-v-3d9d9137="" class="column items" style="width: 33.3333%;"><div data-v-28f55286="" class="card" data-v-3d9d9137="" style="
     border-color: #dc3545 !important;
     border: 2px solid rgba(0,0,0,.125);
@@ -62,8 +64,10 @@
               has-text-weight-light
               ">
             Built For Speed
-          </h3> <h4 style="color: black !important;" data-v-28f55286="" class="subtitle is-6"><span class="author-wrapper"><strong style="color: black !important;">Author:</strong> Daniel Kelly |
-  </span> <span class="date-wrapper"><strong style="color: black !important;">Published on:</strong> August 1, 2019
+          </h3> <h4 style="color: black !important;" data-v-28f55286="" class="subtitle is-6"><span class="author-wrapper"><strong style="color: black !important;">Monthly Dev Discussion!</strong></span> <br>
+<br><span class="date-wrapper"><a class="button is-outlined is-small" style="color: #fff;background-color: #007bff;border-color: #007bff;">
+    Load Comments
+  </a>
   </span></h4></a></div></div></div></div></div></div> <!----> <div data-v-3d9d9137="" class="intersection-observer"></div></div></div></div>
 
 
