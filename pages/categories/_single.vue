4dc9737ec572c28930458304ef6da97f818a73ce
Nicholas Huber <anongenie@gmail.com>
2021-03-21 18:43:25 -0600

    Update _single.vue

--- pages/categories/_single.vue
+++ pages/categories/_single.vue
@@ -12,7 +12,7 @@
       </template>
       <template v-slot:sidebar>
         <h3 class="subtitle">
-          All Categories <div>{{ $route.name }} {{$route}} </div>
+          All Categories <div>{{ $route.name }} </div>
         </h3>
         <div class="panel">
           <nuxt-link
