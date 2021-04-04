8499c983a180ad8a5d6fe92a2c1930c492fdf7f9
Nicholas Huber <anongenie@gmail.com>
2021-04-04 04:00:03 -0600

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -25,8 +25,8 @@ Every time I successfully research and solve a complex issue or create something
            <div class="about-info">
            <img src="https://huber.codes/profilepic-opt.jpg" style="width: 100%" alt="profile-pic">
            <h3 class="mb-20" style="font-size: 22px; font-weight: 700;">Personal Information</h3><br> <ul><li><span class="title2" style="font-size: 16px; font-weight: 700; letter-spacing: 0.5px; border-bottom: 2px solid rgb(255, 255, 255); position: relative; display: inline-block; margin-right: 15px; padding-bottom: 1px; color: rgb(255, 255, 255);">Name</span><span class="value">Nick Huber</span></li> <li><span class="title2" style="font-size: 16px; font-weight: 700; letter-spacing: 0.5px; border-bottom: 2px solid rgb(255, 255, 255); position: relative; display: inline-block; margin-right: 15px; padding-bottom: 1px; color: rgb(255, 255, 255);">Age</span><span class="value">26 Years</span></li> <li><span class="title2" style="font-size: 16px; font-weight: 700; letter-spacing: 0.5px; border-bottom: 2px solid rgb(255, 255, 255); position: relative; display: inline-block; margin-right: 15px; padding-bottom: 1px; color: rgb(255, 255, 255);">Email</span><span class="value">Nick@Huber.codes</span></li></ul></div><br> <img src="https://i.gyazo.com/f471cff490a2a67acc1838d62d863d45.png" style="width: 100%;"> <img src="https://i.gyazo.com/dc874a0dba5eaa68bfb5ee29aefd2196.png" style="width: 100%;"> <br><br> 
-           <center><img src="https://i.imgur.com/Sk0xRxn.png" style="max-width: 200px; text-align: center;">
-          </center>
+           <!--<center><img src="https://i.imgur.com/Sk0xRxn.png" style="max-width: 200px; text-align: center;">
+          </center>-->
           </aside>
           </div>
           </div>
