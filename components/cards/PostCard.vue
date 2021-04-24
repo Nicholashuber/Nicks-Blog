6a6fcf8919f10619ffaac250eb2a32400ccb2501
Nicholas Huber <Nick@huber.dev>
2021-04-24 00:45:57 -0600

    Update PostCard.vue

--- components/cards/PostCard.vue
+++ components/cards/PostCard.vue
@@ -13,12 +13,14 @@
       v-if="author && $siteConfig.posts.displayAuthor"
       class="author-wrapper" style="color: white;"
     >
-    <div v-if="author.indexOf('Portfolio') !== -1">test1</div>
-      <strong style="color: white;">Author:</strong> {{ author }} {{ subtitle }} |
+    <div v-if="author.indexOf('Portfolio') !== -1">{{ subtitle }}</div>
+      <strong style="color: white;">Author:</strong> {{ author }} |
     </span>
+    <div v-if="author.indexOf('Portfolio') === -1">
     <span v-if="date" class="date-wrapper" style="color: white;">
       <strong style="color: white;">Published on:</strong> {{ datePretty }}
     </span>
+    </div>
   </generic-card>
 </template>
 
