d5c47e7798ef51633f430300a81557b8f677b923
Nicholas Huber <Nick@huber.dev>
2021-04-23 22:44:21 -0600

    Update SiteLogo.vue

--- components/SiteLogo.vue
+++ components/SiteLogo.vue
@@ -18,8 +18,8 @@
 <img src="https://huber.codes/huberlogo.png" style="" alt="profile-pic">
     </div>
     <a href="https://calendly.com/nhuber?hide_landing_page_details=1" target="_blank">
-    <div id="nickscalendly" class="calendly-badge-widget" style="margin-left: 54px;">
-    <div class="calendly-badge-content calendly-white" style="background: rgb(255, 255, 255);color: rgb(4, 4, 4) !important;display: table-cell;/* float: left; */width: auto;height: 45px;padding: 0px 24px;border-radius: 25px;box-shadow: rgb(0 0 0 / 25%) 0 2px 5px;font-family: sans-serif;text-align: center;vertical-align: middle;font-weight: bold;font-size: 14px;color: #fff;cursor: pointer;">Schedule time with me</div>
+    <div id="nickscalendly" class="calendly-badge-widget" style="margin-left: 54px;margin-top: 10px;">
+    <div class="calendly-badge-content calendly-white" style="background: rgb(255, 255, 255);color: rgb(4, 4, 4) !important;display: table-cell;/* float: left; */width: auto;height: 33px;padding: 0px 24px;border-radius: 25px;box-shadow: rgb(0 0 0 / 25%) 0 2px 5px;font-family: sans-serif;text-align: center;vertical-align: middle;font-weight: bold;font-size: 14px;color: #fff;cursor: pointer;">Schedule time with me</div>
     </div></a>
   </div>
 </template>
