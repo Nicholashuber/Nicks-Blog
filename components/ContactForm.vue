93b6abc7f8a7d88990e9b048a6e308ec4df720d7
Nicholas Huber <Nick@huber.dev>
2021-05-02 03:12:56 -0600

    Update ContactForm.vue

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form name="contact" method="POST">
+  <form action="https://submit-form.com/V99UzPDl">
     <!-- Name -->
     <div class="field">
       <label class="label is-medium">Name</label>
