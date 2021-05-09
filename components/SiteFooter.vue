49402f27d84845debff604ff97fd07d4b77475c3
Nicholas Huber <anongenie@gmail.com>
2021-05-09 04:22:40 -0600

    Update SiteFooter.vue

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="site-footer">
-    {{ $siteConfig.siteName }} &copy; {{ new Date().getFullYear() }} - All
+    Nick Huber &copy; {{ new Date().getFullYear() }} - All
     Rights Reserved
     <br />
     Made with <span style="color: #e25555;">&#9829;</span> 
