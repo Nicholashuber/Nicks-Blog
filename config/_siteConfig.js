e60ad437995e2318832ca46fe85d1084b94f1374
Nicholas Huber <anongenie@gmail.com>
2021-05-12 00:30:51 -0600

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -15,7 +15,7 @@ export default {
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
     on: true,
-    id: process.env.GOOGLE_ANALYTICS_ID
+    id: 'UA-141591334-2'
   },
   mainMenu: [
     {
